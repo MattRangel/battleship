cb2d9f7cb09a716e3de6ba83483f546936093eb6
Matt Rangel <matt333three@aol.com>
2025-02-10 19:12:55 -0600

    Write test for ship isSunk function

--- src/ship.test.js
+++ src/ship.test.js
@@ -5,3 +5,11 @@ test("hit() adds one to hits property", () => {
   testShip.hit();
   expect(testShip.hits).toBe(1);
 });
+
+test("isSunk() only returns true when hits are equal to length", () => {
+  const testShip = new Ship(2);
+  testShip.hit();
+  expect(testShip.isSunk()).toBe(false);
+  testShip.hit();
+  expect(testShip.isSunk()).toBe(true);
+});
