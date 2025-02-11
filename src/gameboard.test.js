3e012960224d55d37a397933aebf3338928e06b8
Matt Rangel <matt333three@aol.com>
2025-02-10 20:25:14 -0600

    Write tests for side effects of olaceShip function

---
+++ src/gameboard.test.js
@@ -0,0 +1,22 @@
+import Gameboard from "./gameboard";
+import Ship from "./ship";
+
+describe("placeShip", () => {
+  const testGameboard = new Gameboard();
+
+  testGameboard.placeShip({
+    position: [0, 0],
+    direction: "horizontal",
+    length: 2,
+  });
+
+  test("adds ship to ships array", () => {
+    expect(testGameboard.ships.length).toBe(1);
+    expect(testGameboard.ships[0].constructor).toBe(Ship);
+  });
+
+  test("adds ship reference to board 2d array", () => {
+    expect(testGameboard.board[0][0]).toBe({ shipId: 0, hit: false });
+    expect(testGameboard.board[0][1]).toBe({ shipId: 0, hit: false });
+  });
+});
