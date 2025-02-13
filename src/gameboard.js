8fa85d36d6806aff551f35f4c0ea75f34495e747
Matt Rangel <matt333three@aol.com>
2025-02-12 18:09:27 -0600

    Move board array creation to its' own function. Implement placeShip function.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,31 @@
+import Ship from "./ship";
+
 export default class Gameboard {
   constructor() {
     this.ships = [];
-    this.board = [...Array(10)].map(() => Array(10));
+    this.board = this.constructor.#createBoard(10);
+  }
+
+  placeShip({ position, direction, length }) {
+    const shipId = this.ships.push(new Ship(length)) - 1;
+    const spots = this.#getSpots({ position, direction, length });
+    spots.forEach((location) => {
+      const boardSpot = this.board[location[0]][location[1]];
+      boardSpot.shipId = shipId;
+    });
+  }
+
+  #getSpots({ position, direction, length }) {
+    const directionNum = ["vertical", "horizontal"].indexOf(direction);
+
+    return [...Array(length).keys()].map((index) => {
+      const newPosition = [...position];
+      newPosition.splice(directionNum, 1, position[directionNum] + index);
+      return newPosition;
+    });
+  }
+
+  static #createBoard(size) {
+    return [...Array(size)].map(() => [...Array(size)].map(() => new Object()));
   }
 }
